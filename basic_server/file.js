96f22eb5bb8cb92d4efd2bdd0fb2ed9b624dec86
rohit-gupta09 <gupta2002rohit@gmail.com>
2022-07-09 14:51:46 +0530

    Today I have added some basic code of file system using node function

---
+++ basic_server/file.js
@@ -0,0 +1,42 @@
+const fs=require('fs');
+
+
+//reading files
+
+// fs.readFile('./docs/blog1.txt',(err,data)=>{
+//     if(err)
+//     {
+//         console.log(err);
+//     }
+//     else{
+//         console.log(data.toString());
+//     }
+// })
+
+
+
+// Writing Files
+// fs.writeFile('./docs/blog1.txt','Hello World',()=>{
+//     console.log('file was Written');
+// })
+
+// fs.writeFile('./docs/blog2.txt','Hello World',()=>{
+//     console.log('file was Written');
+// })
+
+//directories
+
+// fs.mkdir('./assets',()=>
+// {
+//     if(err)
+//     {
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Folder created")
+//     }
+// })
+
+// unlink command is used to delete the file
+
+
--- basic_server/module.js
+++ basic_server/module.js
@@ -3,3 +3,7 @@ console.log(people);
 
 // console.log(people,age);
 // console.log()
+
+const os=require('os');
+console.log(os.platform(),os.homedir());
+

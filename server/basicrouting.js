2258c048d39386d44be9626cb88463d55585b76b
rohit-gupta09 <gupta2002rohit@gmail.com>
2022-07-11 22:47:14 +0530

    Learn about basic routing using switch case

---
+++ server/basicrouting.js
@@ -0,0 +1,34 @@
+const http=require('http');
+const fs=require('fs');
+
+const server=http.createServer((req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    let path='./views/';
+
+    switch(req.url)
+    {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path,(err,data)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+        else{
+            res.end(data)
+        }
+    })
+});
+
+server.listen(3000,'localhost',()=>{
+    console.log('listening on port number 3000')
+})
